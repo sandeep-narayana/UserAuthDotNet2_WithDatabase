a888dafcd08349d1311ec1ee1eebbf2c79e7c77a
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-09-04 19:19:13 +0530

    implimnet hashing in register and login

--- Repository/AuthRepository.cs
+++ Repository/AuthRepository.cs
@@ -18,34 +18,56 @@ public class AuthRepository : BaseRepository, IAuthRepository
 
     public async Task<bool> CheckAuthentication(UserCredentials userCredentials)
     {
-        var query = "SELECT * FROM users WHERE email = @Email AND password = @Password";
+        var query = "SELECT password FROM users WHERE email = @Email";
 
         var parameters = new
         {
-            Email = userCredentials.Email,
-            Password = userCredentials.Password
+            Email = userCredentials.Email
         };
 
-
         using var con = NewConnection;
-        return await con.QueryFirstOrDefaultAsync(query, parameters) != null;
+        var hashedPasswordFromDb = await con.QueryFirstOrDefaultAsync<string>(query, parameters);
+
+        if (hashedPasswordFromDb != null)
+        {
+            // Use BCrypt to verify if the provided plaintext password matches the stored hashed password.
+            bool isPasswordValid = BCrypt.Net.BCrypt.Verify(userCredentials.Password, hashedPasswordFromDb);
+
+            return isPasswordValid;
+        }
+
+        // Authentication failed.
+        return false;
     }
+
 
     public async Task<bool> RegisterUser(User user)
     {
         // SQL query to insert a new user into the database.
-        var query = "INSERT INTO users (username ,email, password,) VALUES (@Username,@Email, @Password)";
+        var query = "INSERT INTO users (username ,email, password) VALUES (@Username,@Email, @Password)";
+
+        // Hash the password before storing it in the database.
+        var hashedPassword = HashPassword(user.Password);
 
         var parameters = new
         {
             Username = user.Username,
             Email = user.Email,
-            Password = user.Password
+            Password = hashedPassword
         };
 
         using var con = NewConnection;
         await con.ExecuteAsync(query, parameters);
         return true;
 
+    }
+
+    private string HashPassword(string password)
+    {
+        // Generate a salt and hash the password with it using BCrypt.
+        string salt = BCrypt.Net.BCrypt.GenerateSalt(12); // You can adjust the salt work factor (12 in this example).
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+
+        return hashedPassword;
     }
 }
